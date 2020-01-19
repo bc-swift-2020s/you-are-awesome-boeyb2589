2ff3c9f16deaaae10891ae940c2cf997ab371d95
Brandon Boey <brandonjboey@gmail.com>
2020-01-18 22:00:00 -0500

    CH 2 5 Adding Images to an App

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -10,21 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     @IBAction func messageButtonPressed(_ sender: UIButton) {
         messageLabel.text = "You Are Awesome!"
-        messageLabel.textColor = UIColor.blue
-        messageLabel.textAlignment = .left
+        imageView.image = UIImage(named: "image0")
+        
+
     }
-    
-    @IBAction func messageAnotherButtonPressed(_ sender: UIButton) {
-        messageLabel.text = "You Are Great!"
-        messageLabel.textColor = .systemRed
-        messageLabel.textAlignment = .right
-    }
-    
 }
 
