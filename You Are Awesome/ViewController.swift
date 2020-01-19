b661a3f3b6a19bfa6d6bfb7ef206a70ba66239ed
Brandon Boey <brandonjboey@gmail.com>
2020-01-18 21:42:07 -0500

    CH 2 4 Two Button Challenge

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        messageLabel.text = "You Are Awesome!"
+        messageLabel.textColor = UIColor.blue
+        messageLabel.textAlignment = .left
+    }
+    
+    @IBAction func messageAnotherButtonPressed(_ sender: UIButton) {
+        messageLabel.text = "You Are Great!"
+        messageLabel.textColor = .systemRed
+        messageLabel.textAlignment = .right
+    }
+    
 }
 
